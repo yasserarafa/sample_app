c4f790f6057eb0e155e6b85d0ba4d072310ac1fc
Yasser <yasser@yasser-Vostro-1015.(none)>
2013-08-29 11:34:25 +0200

    Finish Ch 7

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
 	def create
 		user = User.find_by(email: params[:session][:email].downcase)
 		if user && user.authenticate(params[:session][:password])
-			sign_in(user)
+			sign_in user
 			redirect_to user
     	else
       		flash[:error] = 'Invalid email/password combination' # Not quite right!
@@ -16,7 +16,7 @@ class SessionsController < ApplicationController
 
 	def destroy
     	sign_out
-    	redirect_to signout_path
+    	redirect_to root_path
   	end
 
 end
