51e293cf3cd437fb9875adfc8a8e3aa164c0b55c
Yasser <yasser@yasser-Vostro-1015.(none)>
2013-09-26 14:05:55 +0200

    sending email with attachments

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,8 +2,10 @@ class UserMailer < ActionMailer::Base
   default from: "yassoraa88@hotmail.com"
 
   def welcome_email()
+    attachments["rails.png"] = File.read('/home/yasser/rails apps/sample_app/app/assets/images/rails.png')
+    
     mail(to: "yassoraa88@gmail.com", subject: '1000 mabrook')
-    attachments['RAILS.PNG'] = File.read('/home/yasser/rails apps/sample_app/app/assets/images/rails.png')
+    
 
   end
 end
